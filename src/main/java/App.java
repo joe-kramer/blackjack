6425b0f55e4d17dcecf0f07cf4567246d9a650c7
Joe Kramer <joekramer1125@gmail.com>
2017-06-29 15:03:25 -0700

    add front end logic through user deciding if they want card or not

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,34 +5,67 @@ import java.util.List;
 public class App {
   public static void main(String[] args) {
     Console myConsole = System.console();
+    boolean play = true;
+    boolean drawLoop = true;
 
     //WELCOME
     System.out.println("Welcome to Blackjack:");
     System.out.println("----------------------------------");
+    myConsole.readLine();
+
+    //NEW GAME
     BlackJack game = new BlackJack();
     game.initializeDeck();
     // System.out.println(game.deck.size());
+    while(play) {
+      //USER PROMPT
+      System.out.println("Enter \"Play\" to deal out another hand, or \"Exit\" to cash out");
+      String userChoice = myConsole.readLine();
+      if (userChoice.equals("Play")) {
+        //USER CARDS
+        List<String> yourDraw = game.userDrawTwoCards();
+        System.out.println("Your first two cards are " + yourDraw.get(0) + ", and " + yourDraw.get(1));
+        System.out.println("Your current total is: " + game.getScore(yourDraw));
+        System.out.println("----------------------------------");
+        myConsole.readLine();
 
-    //USER CARDS
-    // System.out.println(game.deck.get(0));
-    // System.out.println(game.deck.get(1));
-    List<String> yourDraw = game.drawTwoCards();
-    // System.out.println(game.deck.size());
-    // System.out.println(yourDraw);
-    // System.out.println(game.deck.get(0));
-    // System.out.println(game.deck.get(1));
-    System.out.println("Your first two cards are " + yourDraw.get(0) + ", and " + yourDraw.get(1));
-    System.out.println("Your current total is: " + game.getScore(yourDraw));
-    System.out.println("----------------------------------");
+        //DEALER CARDS
+        List<String> dealerDraw = game.dealerDrawTwoCards();
+        System.out.println("The Dealer's first two cards are " + dealerDraw.get(0) + ", and " + dealerDraw.get(1));
+        System.out.println("The Dealer's current total is: " + game.getScore(dealerDraw));
+        System.out.println("----------------------------------");
 
-    //DEALER CARDS
-    // System.out.println(game.deck.get(0));
-    // System.out.println(game.deck.get(1));
-    List<String> dealerDraw = game.drawTwoCards();
-    // System.out.println(game.deck.size());
-    // System.out.println(dealerDraw);
-    System.out.println("The Dealer's first two cards are " + dealerDraw.get(0) + ", and " + dealerDraw.get(1));
-    System.out.println("The Dealer's current total is: " + game.getScore(dealerDraw));
-    System.out.println("----------------------------------");
+        //AFTER INITIAL DEAL
+        while(drawLoop) {
+          System.out.println("Would you like to draw another card? Enter \"Yes\" or \"No\":");
+          String anotherCard = myConsole.readLine();
+          if (anotherCard.equals("Yes")) {
+            yourDraw = game.userDrawOneCard(yourDraw);
+            System.out.println("You drew a " + yourDraw.get((yourDraw.size() - 1)));
+            System.out.println("Your cards are " + yourDraw);
+            System.out.println("Your current total is: " + game.getScore(yourDraw));
+            if(game.getScore(yourDraw) > 21) {
+              System.out.println("Busted!");
+              drawLoop = false;
+            }
+            System.out.println("----------------------------------");
+          } else if (anotherCard.equals("No")) {
+              drawLoop = false;
+          } else {
+            System.out.println("Did not recognize input - Would you like to draw another card? Enter \"Yes\" or \"No\":");
+        }
+      } else if (userChoice.equals("Exit")) {
+        play = false;
+      } else {
+        System.out.println("Did not recognize input - Enter \"Play\" to deal out another hand, or \"Exit\" to cash out");
+      }
+
+      // if (userDidNotLose &) { }
+      // if (game.getScore(dealerDraw) < 17) {
+      //   System.out.println(dealerDraw);
+      //   System.out.println(game.deck.size());
+      // }
+    }
+    System.out.println("Thank you for playing!");
   }
 }
--- src/main/java/BlackJack.java
+++ src/main/java/BlackJack.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
+import java.util.*;
 
 public class BlackJack {
   String[] suits = {"Spades", "Clubs", "Hearts", "Diamonds"};
   String[] values = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
   List<String> deck = new ArrayList<String>();
-  List<String> dealt = new ArrayList<String>();
-  // List<String> randomizedDeck = new ArrayList<String>();
+  List<String> userHand = new ArrayList<String>();
+  List<String> dealerHand = new ArrayList<String>();
 
   public void initializeDeck() {
     for(String suit : suits) {
@@ -18,19 +19,32 @@ public class BlackJack {
     Collections.shuffle(deck);
   }
 
-  public List<String> drawTwoCards() {
-    dealt.clear();
-    dealt.add(deck.get(0));
-    dealt.add(deck.get(1));
-    deck.remove(0);
-    deck.remove(0);
-    return dealt;
+  public List<String> userDrawTwoCards() {
+    userHand.clear();
+    userHand.add(deck.get(0));
+    userHand.add(deck.get(1));
+    deck.subList(0, 2).clear();
+    return userHand;
   }
 
-  public List<String> drawOneCard() {
-    dealt.add(deck.get(0));
+  public List<String> dealerDrawTwoCards() {
+    dealerHand.clear();
+    dealerHand.add(deck.get(0));
+    dealerHand.add(deck.get(1));
+    deck.subList(0, 2).clear();
+    return dealerHand;
+  }
+
+  public List<String> userDrawOneCard(List<String> cards) {
+    userHand.add(deck.get(0));
     deck.remove(0);
-    return dealt;
+    return userHand;
+  }
+
+  public List<String> dealerDrawOneCard(List<String> cards) {
+    dealerHand.add(deck.get(0));
+    deck.remove(0);
+    return dealerHand;
   }
 
   public int getScore(List<String> dealtCards) {
@@ -46,11 +60,16 @@ public class BlackJack {
         total += value;
       }
     }
-    // if (total > 21) {
-    //   if(cardArray[0].equals("Ace")) {
-    //     total -= 10;
-    //   }
-    // }
+    if (total > 21) {
+      for(String card : dealtCards) {
+        String[] cardArray = card.split(" ");
+        if(cardArray[0].equals("Ace")) {
+          do {
+            total -= 10;
+          }while(total > 21)
+        }
+      }
+    }
     return total;
   }
 }
