f0a85735b37b45485622fb4af08cebd371ef952c
Joe Kramer <joekramer1125@gmail.com>
2017-06-29 09:06:55 -0700

    build out full deck

--- src/main/java/BlackJack.java
+++ src/main/java/BlackJack.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class BlackJack {
   String[] suits = {"Spades", "Clubs", "Hearts", "Diamonds"};
-  String[] values = {"Ace", "2"};
+  String[] values = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 
   public List<String> runBlackJack() {
     List<String> deck = new ArrayList<String>();
--- src/test/java/BlackJackTest.java
+++ src/test/java/BlackJackTest.java
@@ -4,19 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BlackJackTest {
+  String[] suits = {"Spades", "Clubs", "Hearts", "Diamonds"};
+  String[] values = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 
   @Test
   public void runBlackJack_createFullDeck_ArrayList() {
     BlackJack testBlackJack = new BlackJack();
     List<String> expectedOutput = new ArrayList<String>();
-    expectedOutput.add("Ace of Spades");
-    expectedOutput.add("2 of Spades");
-    expectedOutput.add("Ace of Clubs");
-    expectedOutput.add("2 of Clubs");
-    expectedOutput.add("Ace of Hearts");
-    expectedOutput.add("2 of Hearts");
-    expectedOutput.add("Ace of Diamonds");
-    expectedOutput.add("2 of Diamonds");
+    for(String suit : suits) {
+      for(String value : values) {
+        expectedOutput.add(value + " of " + suit);
+      }
+    }
     assertEquals(expectedOutput, testBlackJack.runBlackJack());
   }
 }
