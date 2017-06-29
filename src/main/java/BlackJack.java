034dbe567a5710edc59ffc4943d8b30a2345aa55
Joe Kramer <joekramer1125@gmail.com>
2017-06-29 09:01:38 -0700

    build deck and pass test

--- src/main/java/BlackJack.java
+++ src/main/java/BlackJack.java
@@ -2,8 +2,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BlackJack {
+  String[] suits = {"Spades", "Clubs", "Hearts", "Diamonds"};
+  String[] values = {"Ace", "2"};
+
   public List<String> runBlackJack() {
     List<String> deck = new ArrayList<String>();
+    for(String suit : suits) {
+      for(String value : values) {
+        deck.add(value + " of " + suit);
+      }
+    }
     return deck;
   }
 }
